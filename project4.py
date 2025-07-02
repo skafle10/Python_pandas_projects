a544e83c083330e26037252dfdf3e0a86a392d83
skafle10 <kaflesubash.404@gmail.com>
2025-07-02 21:53:49 +0545

    fix: important function

--- project4.py
+++ project4.py
@@ -6,7 +6,7 @@ import pandas as pd
 from rapidfuzz import process
 
 
-FILE_NAME = "customers-100.csv"
+FILE_NAME = "for_project4.csv"
 THRESHOLD = 80
 
 
@@ -23,7 +23,7 @@ def load_data(file_name):
 
 '''Convert the date to datetime object'''
 def to_date_time(df):
-    for cols in pd.select_dtypes(include="objects").columns:
+    for cols in df.select_dtypes(include="object").columns:
         try: 
             df[cols] = pd.to_datetime(df[cols])
 
@@ -35,13 +35,14 @@ def to_date_time(df):
 
 '''clean the data'''
 def clean_text(df):
-        for cols in pd.select_dtypes(include="object").columns:
+        for cols in df.select_dtypes(include="object").columns:
             try:
-                df[cols] = df[cols].str.strip().str.title
+                df[cols] = df[cols].str.strip().str.title()
+
 
 
             except Exception as e:
-             print(f"Error in cleaning the text columns due to: {e}")
+                print(f"Error in cleaning the text columns due to: {e}")
 
         return df
 
@@ -70,18 +71,41 @@ def fuzzy_matcher(df,targets,threshold):
 
 '''Insights based on customers'''
 def top_customers(df):
-    targets = ["Customer id",]
-    
+    targets = ["Customer_ID","Customer_Name","Product","Order_Quantity","Profits"]
+    match = fuzzy_matcher(df,targets,THRESHOLD)
+    print(" The dict so obtained is ")
+    print(match)
+    print(match["Profits"])
+    print(df[match["Profits"]])
+             
+    if match:
+        try: 
+            highest_ordering_customer = df.groupby(match["Customer_ID"])[match["Order_Quantity"]].sum().sort_values(ascending=False)
+            top_purchased_items_individually = df.groupby([match["Customer_ID"],match["Customer_Name"],match["Product"]]) [match["Order_Quantity"]].sum().reset_index().sort_values(by = "Order_Quantity",ascending=False)
+
+            profit_collected_individually = df.groupby([match["Customer_ID"],match["Customer_Name"]])[match["Profits"]].sum().reset_index().sort_values(by="Profits",ascending=False)
+            print(profit_collected_individually)
+
+        except Exception as e:
+            print(f"Error in analyzing top customers due to : {e}")
+
+
+
 
 
 
 
 def main():
+    
     try:
-     load_data(FILE_NAME)
+     df = load_data(FILE_NAME)
 
     except Exception as e:
         print(f"error in calling the data loading function due to: {e}")
+
+    to_date_time(df)
+    clean_text(df)
+    top_customers(df)
 
     
 
