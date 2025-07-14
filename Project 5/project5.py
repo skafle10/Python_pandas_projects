9f1f42e626b4fac4242c92e857a590ddd0218d44
skafle10 <kaflesubash.404@gmail.com>
2025-07-14 22:35:51 +0545

    WIP

--- Project 5/project5.py
+++ Project 5/project5.py
@@ -66,17 +66,42 @@ def clean_dates(df,date):
 
     except Exception as e:
         return f"Error in cleaning the date due tp {e}"
+    
+
+
+def show_basic_info(df,year):
+    total_no_of_movies = len(df)
+    columns_names = list(df.columns)
+    return (f"The number of movies we have here are: {total_no_of_movies}\n"
+           f" from the year {df[year].min()} - {df[year].max()}\n"
+           f"You will get the information about {columns_names}\n"
+
+           )
+
+
+def apply_filters():
+    pass
+
+
+def top_list(df,rating,movie_name):
+    top_rated_movies = df.groupby(movie_name)[rating].sort_values(ascending=False).head()
+    print("The top rated movies are: ")
+    print(top_rated_movies)
 
 
 
 def main():
     df = load_data(FILE_PATH)
-    ready_to_use_columns = fuzzy_matcher(df,TARGETS,THRESHOLD)
-    print("The matched data which are ready to be used are: ")
-    print(ready_to_use_columns)
     clean_string_columns(df)
-    clean_dates(df,ready_to_use_columns["Year"])
+    ready_to_use_columns = fuzzy_matcher(df,TARGETS,THRESHOLD)
 
+    # print("The matched data which are ready to be used are: ")
+    # print(ready_to_use_columns)
+
+    # clean_dates(df,ready_to_use_columns["Year"])
+
+    show_basic_info(df,ready_to_use_columns["Year"])
+    top_list(df,ready_to_use_columns["Rating"],ready_to_use_columns["Title"])
 
 
 if __name__ == "__main__":
