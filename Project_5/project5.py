9b2f4500c51996194002471d27a4de28f8285166
skafle10 <kaflesubash.404@gmail.com>
2025-08-18 23:18:14 +0545

    Final touches left

--- Project_5/project5.py
+++ Project_5/project5.py
@@ -1,19 +1,14 @@
 
 
 # '''Movie  Rating Analyzer'''
-import os
+
 import pandas as pd
 from rapidfuzz import process
-import matplotlib.pyplot as plt
 import streamlit as st
 
 
-# BASE_DIR = os.path.dirname(__file__)
-# FILE_NAME = "movie_ratings_dataset.csv"
-# FILE_PATH = os.path.join(BASE_DIR,FILE_NAME)
 THRESHOLD = 80
 TARGETS = ["Title","Genre","Year","Rating","Votes","Runtime","Country","Language","Review"]
-
 
 
 # '''Loads the file and makes it a dataframe'''
@@ -54,19 +49,19 @@ def fuzzy_matcher(df,targets,threshold):
 def clean_string_columns(df):
     for string_col in df.select_dtypes(include="object").columns:
         try:
-            df[string_col] = string_col.str.strip().str.title()
-            return df
+            df[string_col] = df[string_col].str.strip().str.title()
+            
             
         except Exception as e:
             return f"Errror in cleaning the string columns due to {e}"
         
+    return df
 
 
 # '''cleans the date column from the dataset'''
 def clean_dates(df,date):
     try:
         df[date] = df[date].str.strip().astype(int)      # Convert the year into uniform values, no need to make it a datatime obj in this case
-        print("Date cleaning successful")
         return df
 
     except Exception as e:
@@ -97,6 +92,8 @@ def filter_by_language(df,user_input,language):
 
     except Exception as e:
         return f"Error in filtering by language due to {e}"
+
+
 
 # '''Filters the movies based on the movie genre'''
 def filter_by_genre(df,user_input,genre):
@@ -162,27 +159,52 @@ def year_wise_analysis(df,release_date,rating,movie):
 #'''Display the data analysis using streamlit'''
 
 def apply_analysis(df,ready_to_use_columns):
-    pass
+    analysis_choice = st.radio("How do you want to analyze the dataset?: ",["Year wise","Genre wise"],index=None)
 
+    if (analysis_choice == "Year wise"):
+        st.session_state.branch_view = "year_wise_analysis"
+
+    elif (analysis_choice == "Genre wise"):
+        st.session_state.branch_view = "genre_wise_analysis"
+
+
+    if analysis_choice:
+        if (st.session_state.branch_view == "year_wise_analysis"):
+            average_rating_per_year,total_movies_released_each_year =  year_wise_analysis(df,ready_to_use_columns["Year"],ready_to_use_columns["Rating"],ready_to_use_columns["Title"])
+            st.subheader("The year wise analysis is: ")
+            st.write("The average rating per year is: ")
+            st.write(average_rating_per_year)
+            st.write("The total movies released each year are: ")
+            st.write(total_movies_released_each_year)
+
+
+    if analysis_choice:
+        if (st.session_state.branch_view == "genre_wise_analysis"):
+            average_ratings_per_genre,most_frequent_genres = genre_wise_analysis(df,ready_to_use_columns["Genre"],ready_to_use_columns["Rating"],ready_to_use_columns["Year"])
+            st.subheader("The Genre wise analysis is: ")
+            st.write("Average rating per genre: ")
+            st.write(average_ratings_per_genre)
+            st.write("Genre with highes no of movies: ")
+            st.write(most_frequent_genres)
 
 
 
 #'''apply the filters to the data and show the filtered df using streamlit.'''
 def apply_filter(df,ready_to_use_columns):
 
-    st.subheader("Filtyer Type: ")
-    branch = st.radio("Choose the filter: ",["Genre","Country","Language","Release Date"],index=None)
+    st.subheader("Filter Type: ")
+    filter_choice = st.radio("Choose the filter: ",["Genre","Country","Language","Release Date"],index=None)
 
-    if (branch == "Genre"):
+    if (filter_choice == "Genre"):
         st.session_state.branch_view = "genre_filter"
 
-    elif (branch == "Country"):
+    elif (filter_choice == "Country"):
         st.session_state.branch_view = "country_filter"
 
-    elif(branch == "Language"):
+    elif(filter_choice == "Language"):
         st.session_state.branch_view = "language_filter"
 
-    elif(branch == "Release Date"):
+    elif(filter_choice == "Release Date"):
         st.session_state.branch_view = "releasedate_filter"
 
 
@@ -232,18 +254,27 @@ def apply_filter(df,ready_to_use_columns):
 
 
 
-
-
     if st.session_state.branch_view == "releasedate_filter":
 
         start_date = st.text_input("Enter the start date")
         end_date = st.text_input("Enter the end date: ")
 
         if (start_date and end_date):
-            st.write("The filtered dataset by release date is: ")
-            st.write(filter_by_country(df,start_date,end_date,ready_to_use_columns["Year"]))
-    
+            try:
+                start_date = int(start_date)
+                end_date = int(end_date)
 
+            except Exception as e: 
+                st.write(f"Error converting the date due to {e}")
+
+            if (start_date and end_date in df[ready_to_use_columns["Year"]].unique()):
+
+                st.write("The filtered dataset by release date is: ")
+                st.write(filter_by_release_date(df,start_date,end_date,ready_to_use_columns["Year"]))
+    
+            else:
+                st.write("Please provide a valid year between:\n"
+                         f"{df[ready_to_use_columns["Year"]].min()} and {df[ready_to_use_columns["Year"]].max()}")
 
 
 
@@ -276,47 +307,61 @@ def main():
         
         # st.write(df)
 
-    col1, col2, col3 = st.columns(3)
+    col4,col1, col2, col3 = st.columns(4)
+
+    try:
+            
+        if col4.button("Basic info"):
+            if uploaded_file:
+                st.session_state.main_view = "basic info"
+            
+
+            else:
+                st.warning("Please upload the file first")
 
 
-    if col1.button("Whole DS"):
-        if uploaded_file:
-            st.session_state.main_view = "whole ds"
+        if col1.button("Whole DS"):
+            if uploaded_file:
+                st.session_state.main_view = "whole ds"
 
-        else:
-            st.warning("Please upload the file first!!")
-
-
-    if col2.button("Apply Filter"):
-        if uploaded_file:
-            st.session_state.main_view = "filter ds"
-        else:
-            st.warning("Please upload the file first!!")
+            else:
+                st.warning("Please upload the file first!!")
 
 
-    if col3.button("Analysis"):
-        if uploaded_file:
-            st.session_state.main_view = "analyze"
+        if col2.button("Apply Filter"):
+            if uploaded_file:
+                st.session_state.main_view = "filter ds"
+            else:
+                st.warning("Please upload the file first!!")
 
-        else:
-            st.warning("Please upload the file first!!")
+
+        if col3.button("Analysis"):
+            if uploaded_file:
+                st.session_state.main_view = "analyze"
+
+            else:
+                st.warning("Please upload the file first!!")
+
+
+        
+        if (st.session_state.main_view == "basic info"):
+            st.subheader("The basic info of the given dataset is: ")
+            st.write(show_basic_info(df,ready_to_use_columns["Year"]))
+
+        elif (st.session_state.main_view == "whole ds"):
+            st.subheader("The whole dataset is: ")
+            st.write(df)
+
+        elif (st.session_state.main_view == "filter ds"):
+            apply_filter(df,ready_to_use_columns)
+
+        elif (st.session_state.main_view == "analyze"):
+            apply_analysis(df,ready_to_use_columns)
+
 
     
-    if (st.session_state.main_view == "whole ds"):
-        st.subheader("The whole dataset is: ")
-        st.write(df)
-
-    elif (st.session_state.main_view == "filter ds"):
-        apply_filter(df,ready_to_use_columns)
-
-    elif (st.session_state.main_view == "analyze"):
-        apply_analysis(df,ready_to_use_columns)
-
-
-
-
-
-
+    except Exception as e:
+        st.warning(f"ERROR: {e}")
 
 if __name__ == "__main__":
 
